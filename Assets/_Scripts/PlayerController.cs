418aa4976d1c4db034c547c4e087fa96a8da574f
Shane McDonagh <G00371430@gmit.ie>
2022-03-06 15:24:31 +0000

    Began work on tutorial level design

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     // SerializeField: Can be modified within the editor, while being kept private
     [SerializeField] private float speed = 15.0f;
+    [SerializeField] private float leftBound = -11.8f;
     private float h;
     private float v;
 
@@ -23,17 +24,32 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Movement();
+        processMovement();
+        processAction();
     }
 
-    private void Movement()
+    private void processMovement()
     {
         // Retrieve input from user
         h = Input.GetAxis("Horizontal");
-        Debug.Log("Input was: " + h);
+        // Debug.Log("Input was: " + h);
 
         // Applies velocity to player based on player input
         rigid.velocity = new Vector2(h * speed,0);
+
+        // Prevents the player from going out-of-bounds to the left
+        float bH = Mathf.Clamp(rigid.position.x,leftBound,100);
+
+        // Set position of player based on this
+        rigid.position = new Vector2(bH,rigid.position.y);
     
+    }
+
+    private void processAction()
+    {
+       if(Input.GetButtonDown("Jump"))
+       {
+           // Make the player jump
+       }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MobileAppsProject
 Repository for development of my video game project for 3rd year
+
+## References
+
+## To Note
+- All animation controllers were created by myself, where the provided ones within
+each asset were unused/not imported.
